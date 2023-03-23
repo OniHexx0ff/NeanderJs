941cb55ac1607aa54c28688b62ad239d46417cfd
OniHexx0ff <francis.kaster@solyd.com.br>
2023-03-23 16:10:10 -0300

    don't minify build

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/NeanderJs',
+  build:{
+    minify: false,
+  },
   plugins: [vue()],
   resolve: {
+    extensions: ['.vue', '.js'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
